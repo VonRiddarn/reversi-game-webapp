1f41e280e442e5568298d3fd2149d6e0dfb80c87
VonRiddarn <vonriddarn@gmail.com>
2025-09-26 01:38:22 +0200

    refactor typography styles for headers and improve layout structure in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,12 @@ export default function Home() {
 
 			<h2>Let&apos;s be real though</h2>
 			<p>This is probably a monumental task to try and complete before wednesday. Wish me luck.</p>
+
+			<h3>Header 3 for size test</h3>
+			<h4>Header 4...</h4>
+			<h5>and 5...</h5>
+			<h6>6...</h6>
+			<p>Paragraph</p>
 		</>
 	);
 }
